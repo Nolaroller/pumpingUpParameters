8bad5c248d51c5f274af2e0f43b59d18bed42cab
Nolaroller <dcustard.prsvr@gmail.com>
2023-11-20 08:24:05 -0500

    first comment

---
+++ main.js
@@ -0,0 +1,85 @@
+/******************
+ * YOUR CODE BELOW *
+ ******************/
+let myFirstName = ("Dinah")
+let myLastName = ("Custard")
+let myFavoriteCeleb = ("Anita Baker")
+let mySentence = ("Dinah Custard likes singer Anita Baker")
+let myLuckyNum = 15
+let myAge = 62
+let myHeight = 60
+let myFavoriteYear = 1980
+let mySum = 5 + 2317 + 36
+let myProduct = 232 * 43 / 2+ 4
+let myHeightAndAge = 60 + 62
+
+
+
+
+
+
+/*********************************************************************************************************************
+
+ * CODE BELOW HERE IS FOR INTERNAL USE ONLY! IT ENSURES THAT THE TESTING *
+ * CAN READ AND EVALUATE YOUR CODE. *
+ * DON'T CHANGE ANYTHING BELOW HERE, PLEASE! *
+
+ *********************************************************************************************************************/
+
+if (typeof myFirstName === "undefined") {
+  myFirstName = undefined;
+}
+
+if (typeof myLastName === "undefined") {
+  myLastName = undefined;
+}
+
+if (typeof myFavoriteCeleb === "undefined") {
+  myFavoriteCeleb = undefined;
+}
+
+if (typeof mySentence === "undefined") {
+  mySentence = undefined;
+}
+
+if (typeof myLuckyNum === "undefined") {
+  myLuckyNum = undefined;
+}
+
+if (typeof myAge === "undefined") {
+  myMentalAge = undefined;
+}
+
+if (typeof myHeight === "undefined") {
+  myHeight = undefined;
+}
+
+if (typeof myFavoriteYear === "undefined") {
+  myFavoriteYear = undefined;
+}
+
+if (typeof mySum === "undefined") {
+  mySum = undefined;
+}
+
+if (typeof myProduct === "undefined") {
+  myProduct = undefined;
+}
+
+if (typeof myHeightAndAge === "undefined") {
+  myHeightAndAge = undefined;
+}
+
+module.exports = {
+  myFirstName,
+  myLastName,
+  myFavoriteCeleb,
+  mySentence,
+  myLuckyNum,
+  myAge,
+  myHeight,
+  myFavoriteYear,
+  mySum,
+  myProduct,
+  myHeightAndAge,
+};
